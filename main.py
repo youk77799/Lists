a47252e0f35a6b87bc9fd66a6597708ab4ce6b08
youk77799 <63965544+youk77799@users.noreply.github.com>
2022-01-25 19:48:18 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+friends = ["Mark", "Bucky", "Daniel", "Steve", "dolly"]
+#  index      0       1         2
+print(friends)
+print(friends[0])
+print(friends[-1])
+print(friends[1:3])
+# friend.x()
+# x = remove("name') to remove a specific name
+# x = clear() to clear all name on the lists
+# x = pop() to remove lasted name on the lists
+# print(list.index("name")) to find index
+print(friends.index("Mark"))
+# print(friends.count("name")) to get the number of specific name
+# friends.sort() to sort list ascended
+# .reverse() for opposite result
+
